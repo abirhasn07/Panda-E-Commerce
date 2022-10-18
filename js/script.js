e2f4882c405541dfd95811a7e1c171484e2818ca
abirhasn07 <iamabirhasan07@gmail.com>
2022-10-18 22:03:26 +0600

    swiper js update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict'
+const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay:
+            3500,
+        disableOnInteraction: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+});
